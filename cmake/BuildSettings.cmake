90e15467c466ef34778e116bffb1d5af7a1ae3a9
Yousuke Takada <Yousuke.Takada@grassvalley.com>
2022-01-25 15:32:57 +0900

    Use googletest

--- cmake/BuildSettings.cmake
+++ cmake/BuildSettings.cmake
@@ -8,3 +8,12 @@ if (MSVC)
     add_compile_options(/W4)
     add_compile_options(/permissive-)
 endif ()
+
+# https://google.github.io/googletest/quickstart-cmake.html
+include(FetchContent)
+FetchContent_Declare(
+    googletest
+    URL https://github.com/google/googletest/archive/7eae8de0da5774fa08ce350d9d470901b76b2834.zip)
+# For Windows: Prevent overriding the parent project's compiler/linker settings
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+FetchContent_MakeAvailable(googletest)
--- src/statement_test.cpp
+++ src/statement_test.cpp
@@ -2,45 +2,15 @@
 
 #include "statement.h"
 
+#include <gtest/gtest.h>
+
 namespace {
 
-void assert_equal(const std::string& expected, const std::string& actual)
+TEST(StatementTest, PlainText)
 {
-    if (actual != expected) {
-        throw std::runtime_error{std::format(
-            "strings do not match:\n"
-            "vvvv expected vvvv\n"
-            "{}\n"
-            "^^^^ expected ^^^^\n"
-            "vvvv actual vvvv\n"
-            "{}\n"
-            "^^^^ actual ^^^^\n"s,
-            expected,
-            actual)};
-    }
-
-    assert(actual == expected);
+    EXPECT_EQ(
+        statement("Refactoring"s),
+        "Hello, Refactoring!\n"s);
 }
 
-void statement_test()
-{
-    const auto actual = statement("Refactoring"s);
-    const auto expected = "Hello, Refactoring!\n"s;
-
-    assert_equal(expected, actual);
-}
-
-}
-
-int main()
-{
-    try {
-        statement_test();
-        return 0;
-    }
-    catch (const std::exception& e) {
-        std::cerr << std::format("error: {}\n"s, e.what());
-    }
-
-    return 1;
 }
