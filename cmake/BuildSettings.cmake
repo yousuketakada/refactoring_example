6e9ea3780b7706bdb1e62da9c265dffa15ad0bd5
Yousuke Takada <Yousuke.Takada@grassvalley.com>
2022-01-25 16:39:35 +0900

    Add comments to cmake script

--- cmake/BuildSettings.cmake
+++ cmake/BuildSettings.cmake
@@ -9,11 +9,14 @@ if (MSVC)
     add_compile_options(/permissive-)
 endif ()
 
-# https://google.github.io/googletest/quickstart-cmake.html
+# Declare dependency on GoogleTest
+# It is recommended to update the commit hash often to point to the latest version.
+# See https://google.github.io/googletest/quickstart-cmake.html
 include(FetchContent)
 FetchContent_Declare(
     googletest
-    URL https://github.com/google/googletest/archive/7eae8de0da5774fa08ce350d9d470901b76b2834.zip)
+    URL https://github.com/google/googletest/archive/7eae8de0da5774fa08ce350d9d470901b76b2834.zip
+)
 # For Windows: Prevent overriding the parent project's compiler/linker settings
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 FetchContent_MakeAvailable(googletest)
