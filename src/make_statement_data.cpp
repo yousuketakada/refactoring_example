e038b5925443cf0cfd762ef70b68c17d50735c34
Yousuke Takada <Yousuke.Takada@grassvalley.com>
2022-01-28 08:02:39 +0900

    Extract make_statement_data() to separate source: make_statement_data.{h,cpp}

---
+++ src/make_statement_data.cpp
@@ -0,0 +1,76 @@
+#include "pch.h"
+
+#include "make_statement_data.h"
+
+StatementData make_statement_data(const Invoice& invoice, const std::map<std::string, Play>& plays)
+{
+    auto play_for = [&](const auto& perf) -> decltype(auto)
+    {
+        return plays.at(perf.play_id);
+    };
+
+    auto amount_for = [&](const auto& perf)
+    {
+        int amount = 0;
+        switch (perf.play.type) {
+        case Play::Type::Tragedy:
+            amount = 40000;
+            if (perf.base.audience > 30) {
+                amount += 1000 * (perf.base.audience - 30);
+            }
+            break;
+        case Play::Type::Comedy:
+            amount = 30000;
+            if (perf.base.audience > 20) {
+                amount += 10000 + 500 * (perf.base.audience - 20);
+            }
+            amount += 300 * perf.base.audience;
+            break;
+        default:
+            throw std::runtime_error{std::format(
+                "unknown type: {}"s,
+                static_cast<int>(perf.play.type))};
+        }
+        return amount;
+    };
+
+    auto volume_credits_for = [&](const auto& perf)
+    {
+        int volume_credits = 0;
+        volume_credits += std::max(perf.base.audience - 30, 0);
+        if (Play::Type::Comedy == perf.play.type) { volume_credits += perf.base.audience / 5; }
+        return volume_credits;
+    };
+
+    auto enrich_performance = [&](const auto& base)
+    {
+        EnrichedPerformance enriched{
+            .base = base,
+            .play = play_for(base)
+        };
+
+        enriched.amount = amount_for(enriched);
+        enriched.volume_credits = volume_credits_for(enriched);
+
+        return enriched;
+    };
+
+    std::vector<EnrichedPerformance> enriched_performances;
+    std::ranges::copy(
+        invoice.performances | std::views::transform(enrich_performance),
+        std::back_inserter(enriched_performances));
+
+    auto total_amount = std::accumulate(
+        std::cbegin(enriched_performances), std::cend(enriched_performances),
+        0, [](int sum, const auto& perf) { return sum + perf.amount; });
+    auto total_volume_credits = std::accumulate(
+        std::cbegin(enriched_performances), std::cend(enriched_performances),
+        0, [](int sum, const auto& perf) { return sum + perf.volume_credits; });
+
+    return StatementData{
+        .customer = invoice.customer,
+        .performances = std::move(enriched_performances),
+        .total_amount = total_amount,
+        .total_volume_credits = total_volume_credits
+    };
+}
---
+++ src/make_statement_data.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "statement.h"
+
+struct EnrichedPerformance
+{
+    const Invoice::Performance& base;
+    const Play& play;
+    int amount;
+    int volume_credits;
+};
+
+struct StatementData
+{
+    const std::string& customer;
+    std::vector<EnrichedPerformance> performances;
+    int total_amount;
+    int total_volume_credits;
+};
+
+StatementData make_statement_data(const Invoice& invoice, const std::map<std::string, Play>& plays);
