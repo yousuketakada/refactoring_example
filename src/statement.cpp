1d77be161396e8f2e4393efa491cb8325c526293
Yousuke Takada <Yousuke.Takada@grassvalley.com>
2022-01-28 07:51:58 +0900

    No need to supply defaults when using designated initializers
    See https://en.cppreference.com/w/cpp/language/aggregate_initialization

--- src/statement.cpp
+++ src/statement.cpp
@@ -16,16 +16,16 @@ struct EnrichedPerformance
 {
     const Invoice::Performance& base;
     const Play& play;
-    int amount{};
-    int volume_credits{};
+    int amount;
+    int volume_credits;
 };
 
 struct StatementData
 {
     const std::string& customer;
     std::vector<EnrichedPerformance> performances;
-    int total_amount{};
-    int total_volume_credits{};
+    int total_amount;
+    int total_volume_credits;
 };
 
 StatementData make_statement_data(const Invoice& invoice, const std::map<std::string, Play>& plays)
