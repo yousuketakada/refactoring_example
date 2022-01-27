a730ae622d269c03dad8b56852756530c309f753
Yousuke Takada <Yousuke.Takada@grassvalley.com>
2022-01-27 23:31:02 +0900

    Combine Functions into Transform: Add amount and volume_credits to EnrichedPerformance

--- src/statement.cpp
+++ src/statement.cpp
@@ -16,6 +16,8 @@ struct EnrichedPerformance
 {
     const Invoice::Performance& base;
     const Play& play;
+    int amount{};
+    int volume_credits{};
 };
 
 struct StatementData
@@ -26,6 +28,45 @@ struct StatementData
 
 std::string render_plain_text(const StatementData& data)
 {
+    auto total_amount = [&]()
+    {
+        int total = 0;
+        for (const auto& perf : data.performances) {
+            total += perf.amount;
+        }
+        return total;
+    };
+
+    auto total_volume_credits = [&]()
+    {
+        int total = 0;
+        for (const auto& perf : data.performances) {
+            total += perf.volume_credits;
+        }
+        return total;
+    };
+
+    std::ostringstream oss;
+    oss << std::format("Statement for {}\n"s, data.customer);
+
+    for (const auto& perf : data.performances) {
+        oss << std::format("  {}: {} ({} seats)\n"s, perf.play.name, usd(perf.amount), perf.base.audience);
+    }
+
+    oss << std::format("Amount owed is {}\n"s, usd(total_amount()));
+    oss << std::format("You earned {} credits\n"s, total_volume_credits());
+    return std::move(oss).str();
+}
+
+}
+
+std::string statement(const Invoice& invoice, const std::map<std::string, Play>& plays)
+{
+    auto play_for = [&](const auto& perf) -> decltype(auto)
+    {
+        return plays.at(perf.play_id);
+    };
+
     auto amount_for = [&](const auto& perf)
     {
         int amount = 0;
@@ -57,51 +98,17 @@ std::string render_plain_text(const StatementData& data)
         return volume_credits;
     };
 
-    auto total_amount = [&]()
-    {
-        int total = 0;
-        for (const auto& perf : data.performances) {
-            total += amount_for(perf);
-        }
-        return total;
-    };
-
-    auto total_volume_credits = [&]()
-    {
-        int total = 0;
-        for (const auto& perf : data.performances) {
-            total += volume_credits_for(perf);
-        }
-        return total;
-    };
-
-    std::ostringstream oss;
-    oss << std::format("Statement for {}\n"s, data.customer);
-
-    for (const auto& perf : data.performances) {
-        oss << std::format("  {}: {} ({} seats)\n"s, perf.play.name, usd(amount_for(perf)), perf.base.audience);
-    }
-
-    oss << std::format("Amount owed is {}\n"s, usd(total_amount()));
-    oss << std::format("You earned {} credits\n"s, total_volume_credits());
-    return std::move(oss).str();
-}
-
-}
-
-std::string statement(const Invoice& invoice, const std::map<std::string, Play>& plays)
-{
-    auto play_for = [&](const auto& perf) -> decltype(auto)
-    {
-        return plays.at(perf.play_id);
-    };
-
     auto enrich_performance = [&](const auto& base)
     {
-        return EnrichedPerformance{
+        EnrichedPerformance enriched{
             .base = base,
             .play = play_for(base)
         };
+
+        enriched.amount = amount_for(enriched);
+        enriched.volume_credits = volume_credits_for(enriched);
+
+        return enriched;
     };
 
     std::vector<EnrichedPerformance> enriched_performances;
