93e565d53c1665307621597703ad880cb8ae6030
Yousuke Takada <Yousuke.Takada@grassvalley.com>
2022-05-20 23:11:38 +0900

    Make variables const: total_amount, total_volume_credits

--- src/make_statement_data.cpp
+++ src/make_statement_data.cpp
@@ -94,14 +94,14 @@ StatementData make_statement_data(const Invoice& invoice, const std::map<std::st
         std::back_inserter(enriched_performances));
     assert(std::size(enriched_performances) == std::size(invoice.performances));
 
-    auto total_amount = std::accumulate(
+    const auto total_amount = std::accumulate(
         std::cbegin(enriched_performances), std::cend(enriched_performances),
         0, [](int sum, const auto& perf) { return sum + perf.amount; });
-    auto total_volume_credits = std::accumulate(
+    const auto total_volume_credits = std::accumulate(
         std::cbegin(enriched_performances), std::cend(enriched_performances),
         0, [](int sum, const auto& perf) { return sum + perf.volume_credits; });
 
-    return StatementData{
+    return {
         .customer = invoice.customer,
         .performances = std::move(enriched_performances),
         .total_amount = total_amount,
