830817c6d8bc4ec6a354a815caf2f6b98ca6df04
Yousuke Takada <yousuketakada@gmail.com>
2022-09-04 22:32:06 +0900

    Fix format string to be ""sv

--- src/statement.cpp
+++ src/statement.cpp
@@ -33,21 +33,21 @@ auto render_plain_text(const StatementData& data)
 auto render_html(const StatementData& data)
 {
     std::ostringstream oss;
-    oss << std::format("<h1>Statement for {}</h1>\n"s, data.customer);
+    oss << std::format("<h1>Statement for {}</h1>\n"sv, data.customer);
 
     oss << "<table>\n"s;
     oss << "  <tr><th>play</th><th>seats</th><th>cost</th></tr>\n"s;
 
     for (const auto& perf : data.performances) {
         oss << std::format(
-            "  <tr><td>{}</td><td>{}</td><td>{}</td></tr>\n"s,
+            "  <tr><td>{}</td><td>{}</td><td>{}</td></tr>\n"sv,
             perf.play.name, perf.base.audience, usd(perf.amount));
     }
 
     oss << "</table>\n"s;
 
-    oss << std::format("<p>Amount owed is <em>{}</em></p>\n"s, usd(data.total_amount));
-    oss << std::format("<p>You earned <em>{}</em> credits</p>\n"s, data.total_volume_credits);
+    oss << std::format("<p>Amount owed is <em>{}</em></p>\n"sv, usd(data.total_amount));
+    oss << std::format("<p>You earned <em>{}</em> credits</p>\n"sv, data.total_volume_credits);
     return std::move(oss).str();
 }
 
